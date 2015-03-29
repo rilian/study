e2415f63c33bdf5009469f334582e69b4e96db5a
rilian <dmitriyis@gmail.com>
2015-03-29 15:22:41 +0300

    improve record saving

--- app.rb
+++ app.rb
@@ -18,12 +18,21 @@ get '/' do
 end
 
 post '/records' do
-  params[:records].split(/[\n\r]+/).each do |r|
-    puts "PARAMS url=#{r} type=#{params[:type]}"
+  params[:records].split(/\s+/).each do |r|
+    r = "http://#{r}" unless r.index('http') == 0
     record = Record.new(url: r, type: params[:type])
     if !record.save
       halt 422, record.errors.full_messages.inspect
     end
   end
   redirect '/'
+end
+
+patch '/records/:id' do
+  record = Record.find(params[:id])
+  if record.update(seen: params[:seen] == 'true')
+    'OK'
+  else
+    halt 422, record.errors.full_messages.inspect
+  end
 end
